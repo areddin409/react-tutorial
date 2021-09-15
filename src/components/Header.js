b6e1b57d80ae9860227573acc1218fc2fb073967
areddin409 <areddin409@gmail.com>
2021-09-15 11:05:57 -0500

    Completed UI for Task Tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('Click');
-  };
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      {showAdd ? (
+        <Button color='red' text='Close' onClick={onAdd} />
+      ) : (
+        <Button color='green' text='Add' onClick={onAdd} />
+      )}
     </header>
   );
 };
